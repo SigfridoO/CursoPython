b804f9b2dc3ddcda7bbdce807d2386c070ba1353
Sigfrido <sigfrido_o@ardilluda.com.mx>
2023-09-01 07:52:34 -0600

    se crea la carpeta del proyecto

---
+++ main.py
@@ -0,0 +1,18 @@
+print("================= variables ===============")
+
+print('hola mundo')
+a = 5
+print("a:", a)
+
+b = 4.342
+print('b:', b)
+
+print("a, b:", a, b)
+
+opcion = True
+print('opcion:', opcion)
+
+
+print("================= instrucciones de control (if-else) ===============")
+
+
