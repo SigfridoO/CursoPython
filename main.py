11c0c10aeab385ec6fa2f3e4a88faaa4044a3b0d
Sigfrido <sigfrido_o@ardilluda.com.mx>
2023-09-01 07:59:02 -0600

    se empieza con if-else

--- main.py
+++ main.py
@@ -15,4 +15,11 @@ print('opcion:', opcion)
 
 print("================= instrucciones de control (if-else) ===============")
 
+c = 2
+
+if c > 5:
+    print('c es mayor a 5')
+else:
+    print('c NO es mayor a 5')
+
 
