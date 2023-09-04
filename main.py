6cae165a9bf27308de150612f7d77ea9dc45758e
Sigfrido <sigfrido_o@ardilluda.com.mx>
2023-09-04 07:29:40 -0600

    Se ven las listas mas a profundidad

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from Funciones import nuevoTema
 
 
 
-
+# -----------------  variables
 nuevoTema('variables')
 
 print('hola mundo')
@@ -18,7 +18,7 @@ opcion = True
 print('opcion:', opcion)
 
 
-
+# -----------------  if-else
 nuevoTema('instrucciones de control (if-else)')
 c = 2
 
@@ -30,7 +30,7 @@ else:
     print('c NO es mayor a 5')
 
 
-
+# -----------------  listas
 nuevoTema('listas')
 frutas = ['piñas', 'peras', 'manzanas', 'platanos']
 print('frutas', frutas)
@@ -38,8 +38,18 @@ print('frutas', frutas)
 varios = ['zapatos', 3, 78.45, True, frutas]
 print('varios: ', varios)
 
+print('\n')
+alumnos = ['Oswaldo', 'Carlos', 'Carolina', 'Johana', 'Karla', 'Alan', 'Alejandra', 'Eduardo', 'Luis']
+print('alumnos', alumnos)
 
+print('alumnos[4]: ', alumnos[4])       # Selecciona el objeto con índice 4
+print('alumnos[1:3]:', alumnos[1:3])    # Selecciona el objeto desde el 1 hasta el 2
+print('alumnos[:]:', alumnos[:])        # Dame todos los alumnos
+print('alumnos[-1]:', alumnos[-1])      # Dame el último elemento de la lista
+print('alumnos[-2]:', alumnos[-2])      # Dame el penultimo elemento de la lista
+print('alumnos[1:8:2]', alumnos[1:8:2]) # Dame los elementos del 1 al 7 pero con un salto del 2
 
+# -----------------  for
 nuevoTema('Instrucciones de control - for')
 for fruta in frutas:
     print(fruta)
