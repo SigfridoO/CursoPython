1dbcf137475f5f91376656e63065f3a4c10882e5
Sigfrido <sigfrido_o@ardilluda.com.mx>
2023-09-15 13:33:04 -0600

    En la carpeta interfaz gráfica se crea boton

--- InterfazGrafica/programa1.py
+++ InterfazGrafica/programa1.py
@@ -1,17 +1,24 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
 
-
-class MainWindow(QMainWindow):
+class Ventana(QMainWindow):
     def __init__(self):
         #super().__init__()
         QMainWindow.__init__(self)
+        # Propiedades de la ventana
+        self.resize(500, 200)
+        self.setMinimumSize(300, 100)
+        self.setMaximumSize(600, 500)
+        self.setWindowTitle('Mi primera interfaz gráfica')
 
-        self.resize(300, 400)
+        # Boton
+        boton = QPushButton('presioname')
 
+        # El boton se agrega a la ventana principal
+        self.setCentralWidget(boton)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = MainWindow()
+    window = Ventana()
     window.show()
     sys.exit(app.exec())
