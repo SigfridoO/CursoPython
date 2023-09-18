35997b0c8195b5a86966bd3d6fffa3ffa004079b
Sigfrido <sigfrido_o@ardilluda.com.mx>
2023-09-18 00:46:55 -0600

    actualizando

--- Controladora/Controladora.py
+++ Controladora/Controladora.py
@@ -3,37 +3,29 @@ __date__ = "$25-ene-2020 12:43:00$"
 
 import os
 import platform
-import sys
-import threading
-import time
-from datetime import datetime, timedelta
 
 from blessed import Terminal
-
 
 term = Terminal()
 
 sistema = platform.system()
 plataforma = platform.uname()
-version = ""
 
-caracterDirectorio = ""
 if sistema == "Windows":
-    caracterDirectorio = '\\'
+    print('Estamos en Windows')
 elif sistema == "Linux":
-    caracterDirectorio = '/'
+    print('Estamos en Linux')
     if plataforma.node == "raspberrypi":
-        version = plataforma.node
+        print('Es una raspberry')
         import RPi.GPIO as GPIO
+
 
 class Controladora:
     def __init__(self):
         print('Iniciando controladora')
 
+        if plataforma.node == 'raspberrypi':
 
-
-        if version == 'raspberrypi':
-            print('Es una raspberry')
             GPIO.setmode(GPIO.BCM)
             GPIO.setwarnings(False)
 
@@ -47,17 +39,12 @@ class Controladora:
             GPIO.setup(self.DO_00, GPIO.OUT)
             GPIO.setup(self.DO_01, GPIO.OUT)
 
-
-            #GPIO.output(self.DO_00, True)
-            #self.boton01 = GPIO.input(self.DI_00)
-            #print(self.boton01)
-
+            # GPIO.output(self.DO_00, True)
+            # self.boton01 = GPIO.input(self.DI_00)
+            # print(self.boton01)
 
             while True:
                 GPIO.output(self.DO_00, GPIO.input(self.DI_00))
-
-
-
 
 
 def main():
