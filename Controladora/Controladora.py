8f1ed12622c8e128904c766871c2120497a14b17
Sigfrido <sigfrido_o@ardilluda.com.mx>
2023-09-20 14:46:46 -0600

    Creando la clase temporizador

--- Controladora/Controladora.py
+++ Controladora/Controladora.py
@@ -1,31 +1,20 @@
-__author__ = "Sigfrido"
-__date__ = "$25-ene-2020 12:43:00$"
-
-import os
 import platform
-
-from blessed import Terminal
-
-term = Terminal()
-
 sistema = platform.system()
 plataforma = platform.uname()
 
 if sistema == "Windows":
     print('Estamos en Windows')
-elif sistema == "Linux":
+elif sistema=='Linux':
     print('Estamos en Linux')
-    if plataforma.node == "raspberrypi":
+    if plataforma.node == "raspberrypy":
         print('Es una raspberry')
         import RPi.GPIO as GPIO
 
-
 class Controladora:
     def __init__(self):
-        print('Iniciando controladora')
+        print('Iniciando la controladora')
 
-        if plataforma.node == 'raspberrypi':
-
+        if plataforma.node == "raspberrypy":
             GPIO.setmode(GPIO.BCM)
             GPIO.setwarnings(False)
 
@@ -34,22 +23,18 @@ class Controladora:
 
             self.DI_00 = 22
 
-            GPIO.setup(self.DI_00, GPIO.IN)
-
             GPIO.setup(self.DO_00, GPIO.OUT)
             GPIO.setup(self.DO_01, GPIO.OUT)
 
-            # GPIO.output(self.DO_00, True)
-            # self.boton01 = GPIO.input(self.DI_00)
-            # print(self.boton01)
+            GPIO.setup(self.DI_00, GPIO.IN)
 
             while True:
+
                 GPIO.output(self.DO_00, GPIO.input(self.DI_00))
 
 
 def main():
     controladora = Controladora()
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
---
+++ REAME.md
@@ -0,0 +1,1 @@
+https://gist.github.com/SigfridoO/50d31926f8246ebf313f2659b63ec268
