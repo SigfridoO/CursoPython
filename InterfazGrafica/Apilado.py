026635c4e861f9abd3cd775b40bf84aacc47806a
Sigfrido <sigfrido_o@ardilluda.com.mx>
2023-10-11 13:24:49 -0600

    actualizando

--- InterfazGrafica/Apilado.py
+++ InterfazGrafica/Apilado.py
@@ -18,16 +18,22 @@ class Ventana(QMainWindow):
         self.layout = layout
         self.setCentralWidget(layout)
 
+
+
+
     def keyPressEvent(self, event) -> None:
-        print('Tecla presionada')
+        print('Tecla presionada', event.key())
 
         indice = self.layout.currentIndex()
         indiceMaximo = self.layout.count()-1
 
         if event.key() == Qt.Key_Right:
+            print('Se presiono la tecla derecha')
 
-        event.accept()
-
+        if event.key() == Qt.Key_Left:
+            print('Se presiono la tecla izquierda')
+        # event.accept()
+#https://doc.qt.io/qtforpython-5/PySide2/QtCore/Qt.html
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ventana = Ventana()
