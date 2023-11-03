ad6eb41c15abf75694a05263ad543bfa9f7a8e51
Sigfrido <sigfrido_o@ardilluda.com.mx>
2023-11-03 07:40:05 -0600

    actualizando

--- Semaforo/ControladoraRasp.py
+++ Semaforo/ControladoraRasp.py
@@ -3,6 +3,7 @@ __date__ = "14-mar-2023 21:10:00"
 
 import platform
 import threading
+import serial
 
 sistema = platform.system()
 plataforma = platform.uname()
@@ -18,13 +19,33 @@ elif sistema == 'Linux':
 
 class Controladora:
     def __init__(self):
-        self.DI_03 = None
-        self.DI_02 = None
+
         print('Iniciando la controladora')
 
+        self.DI_01 = None
+        self.DI_00 = None
+        self.DI_02 = None
+        self.DI_03 = None
+        self.DI_04 = None
+        self.DI_05 = None
+        self.DI_06 = None
+        self.DI_07 = None
 
-
-
+        self.DO_00 = None
+        self.DO_01 = None
+        self.DO_02 = None
+        self.DO_03 = None
+        self.DO_04 = None
+        self.DO_05 = None
+        self.DO_06 = None
+        self.DO_08 = None
+        self.DO_07 = None
+        self.DO_09 = None
+        self.DO_10 = None
+        self.DO_11 = None
+        self.DO_12 = None
+        self.DO_13 = None
+        self.DO_14 = None
 
         # Entradas Digitales
         self.X_00 = False
@@ -35,7 +56,6 @@ class Controladora:
         self.X_05 = False
         self.X_06 = False
         self.X_07 = False
-
 
         # Salidas digitales
         self.Y_00 = False
@@ -58,11 +78,8 @@ class Controladora:
         self.Y_14 = False
         self.Y_15 = False
 
-
-
-
-        self.instruccionLeida=""
-        self.instruccionEscrita=""
+        self.instruccionLeida = ""
+        self.instruccionEscrita = ""
 
         self.estado = False
 
@@ -71,7 +88,6 @@ class Controladora:
 
             tarea1 = threading.Thread(target=self.loop)
             tarea1.start()
-
 
     def activarPin(self, direccion, valor):
         if plataforma.node == "raspberrypi":
@@ -88,32 +104,27 @@ class Controladora:
             if direccion == 3:
                 GPIO.output(self.DO_03, valor)
 
-
     def configurarSenales(self):
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
 
-
-
-
         # --------------  Entradas
-        #Botones 
+        # Botones
 
         self.DI_00 = 18
         self.DI_01 = 23
         self.DI_02 = 24
         self.DI_03 = 25
 
-        #Sensores
+        # Sensores
 
         self.DI_04 = 8
         self.DI_05 = 7
         self.DI_06 = 12
         self.DI_07 = 16
 
-
         # --------------  Salidas
-        #Lamparas
+        # Lamparas
         self.DO_00 = 4
         self.DO_01 = 17
         self.DO_02 = 27
@@ -142,7 +153,7 @@ class Controladora:
         GPIO.setup(self.DI_01, GPIO.IN)
         GPIO.setup(self.DI_02, GPIO.IN)
         GPIO.setup(self.DI_03, GPIO.IN)
-        
+
         GPIO.setup(self.DI_04, GPIO.IN)
         GPIO.setup(self.DI_05, GPIO.IN)
         GPIO.setup(self.DI_06, GPIO.IN)
@@ -164,25 +175,18 @@ class Controladora:
         GPIO.setup(self.DO_10, GPIO.OUT)
         GPIO.setup(self.DO_11, GPIO.OUT)
 
-
-        GPIO.setup(self.DI_12, GPIO.IN)
-        GPIO.setup(self.DI_13, GPIO.IN)
-        GPIO.setup(self.DI_14, GPIO.IN)
-        # GPIO.setup(self.DI_15, GPIO.IN)
-
+        GPIO.setup(self.DO_12, GPIO.OUT)
+        GPIO.setup(self.DO_13, GPIO.OUT)
+        GPIO.setup(self.DO_14, GPIO.OUT)
+        # GPIO.setup(self.DO_15, GPIO.IN)
 
         # configurar puerto serie
 
     def loop(self):
         self.estado = True
         while self.estado:
-            
-
             self.leerYEscribirPines()
-            #recibirDatos()
-
-            
-
+            # recibirDatos()
 
     def leerYEscribirPines(self):
         # Entradas digitales
@@ -212,24 +216,24 @@ class Controladora:
         GPIO.output(self.DO_12, self.Y_12)
         GPIO.output(self.DO_13, self.Y_13)
         GPIO.output(self.DO_14, self.Y_14)
-        GPIO.output(self.DO_15, self.Y_15)
-
-        GPIO.output(self.DO_16, self.Y_16)
-        GPIO.output(self.DO_17, self.Y_17)
-        GPIO.output(self.DO_18, self.Y_18)
-        GPIO.output(self.DO_19, self.Y_19)
-        GPIO.output(self.DO_20, self.Y_20)
-        GPIO.output(self.DO_21, self.Y_21)
-        GPIO.output(self.DO_22, self.Y_22)
-        GPIO.output(self.DO_23, self.Y_23)
-
+        # GPIO.output(self.DO_15, self.Y_15)
+        #
+        # GPIO.output(self.DO_16, self.Y_16)
+        # GPIO.output(self.DO_17, self.Y_17)
+        # GPIO.output(self.DO_18, self.Y_18)
+        # GPIO.output(self.DO_19, self.Y_19)
+        # GPIO.output(self.DO_20, self.Y_20)
+        # GPIO.output(self.DO_21, self.Y_21)
+        # GPIO.output(self.DO_22, self.Y_22)
+        # GPIO.output(self.DO_23, self.Y_23)
 
     def recibirDatos(self):
         # self.instruccionLeida = serial.read(50)
         pass
 
-    def transmitirDatos(instruccion):
+    def transmitirDatos(self, instruccion):
         serial.write(50)
+
 
 def main():
     controladora = Controladora()
